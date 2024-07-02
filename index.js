b5deae4b299004703e95571749e146165e7a106d
James Adedini <145149678+ADEDINIJAMES@users.noreply.github.com>
2024-07-03 00:14:29 +0100

    personal website commit first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function updateTimeAndDay() {
+    const currentTimeElement = document.querySelector('[data-testid="currentTimeUTC"]');
+    const currentDayElement = document.querySelector('[data-testid="currentDay"]');
+
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = daysOfWeek[now.getUTCDay()];
+
+    currentTimeElement.textContent = `Current Time (UTC): ${utcTime}`;
+    currentDayElement.textContent = `Current Day: ${currentDay}`;
+}
+
+document.addEventListener('DOMContentLoaded', updateTimeAndDay);
